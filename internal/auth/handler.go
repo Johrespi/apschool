bf14f93bfbd8be904176120d8eaa907e06d6aba8
Johrespi <johrespi@espol.edu.ec>
2025-11-29 22:39:33 -0500

    basic auth handler

---
+++ internal/auth/handler.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(service *Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) GetMe(w http.ResponseWriter, r *http.Request) {
+	userID := 1
+
+	user, err := h.service.GetUserByID(r.Context(), userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
