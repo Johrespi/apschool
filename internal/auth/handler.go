4da1b481052a6511a536b46d9919efda597cbe25
Johrespi <johrespi@espol.edu.ec>
2025-11-29 23:35:58 -0500

    wip: github login

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -2,7 +2,9 @@ package auth
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 type Handler struct {
@@ -11,6 +13,37 @@ type Handler struct {
 
 func NewHandler(service *Service) *Handler {
 	return &Handler{service: service}
+}
+
+func (h *Handler) GithubLogin(w http.ResponseWriter, r *http.Request) {
+	clientID := os.Getenv("GITHUB_CLIENT_ID")
+	redirectURI := os.Getenv("GITHUB_REDIRECT_URI")
+
+	url := fmt.Sprintf(
+		"https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=user:email",
+		clientID,
+		redirectURI,
+	)
+
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func (h *Handler) GithubCallback(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "code not found", http.StatusBadRequest)
+		return
+	}
+
+	// TODO:
+	// 1. Intercambiar code por access_token
+	// 2. Obtener datos del usuario de GitHub API
+	// 3. Crear/obtener usuario en nuestra DB
+	// 4. Generar JWT
+	// 5. Redirigir al frontend con el token
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"code": code})
 }
 
 func (h *Handler) GetMe(w http.ResponseWriter, r *http.Request) {
