187931fcd0ac4010b3cce3467d24419418bb91cf
Johrespi <johrespi@espol.edu.ec>
2025-12-02 00:41:00 -0500

    basic errors.go helper

---
+++ internal/response/errors.go
@@ -0,0 +1,22 @@
+package response
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func ErrorResponse(w http.ResponseWriter, status int, message any) {
+	err := WriteJSON(w, status, map[string]any{"error": message}, nil)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+}
+
+func ServerError(w http.ResponseWriter, r *http.Request, logger *slog.Logger, err error) {
+	logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
+	ErrorResponse(w, http.StatusInternalServerError, "internal server error")
+}
+
+func ValidationError(w http.ResponseWriter, errors map[string]string) {
+	ErrorResponse(w, http.StatusUnprocessableEntity, errors)
+}
