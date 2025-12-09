e107fb773241f124f539e1ac2b2416a6ff7ee3f0
Johrespi <johrespi@espol.edu.ec>
2025-12-09 17:55:08 -0500

    wip: seed script readfile func

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
@@ -24,4 +28,76 @@ type Challenge struct {
 	Template    string
 	TestCode    string
 	Hints       string
+}
+
+func main() {
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDB() (*sql.DB, error) {
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", username, password, host, port, database, schema)
+
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func loadChallenges(basePath string) ([]Challenge, error) {
+	var challenges []Challenge
+
+	categories, err := os.ReadDir(basePath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, category := range categories {
+		if !category.IsDir() {
+			continue
+		}
+
+		categoryPath := filepath.Join(basePath, category.Name())
+		slugs, err := os.ReadDir(categoryPath)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, slug := range slugs {
+			if !slug.IsDir() {
+				continue
+			}
+
+			challengePath := filepath.Join(categoryPath, slug.Name())
+			challenge, err := loadChallenge(challengePath, category.Name(), slug.Name())
+			if err != nil {
+				log.Printf("Warning: skipping %s: %v", challengePath, err)
+				continue
+			}
+
+			challenges = append(challenges, challenge)
+		}
+	}
+
+	return challenges, nil
+}
+
+func loadChallenge(path, category, slug string) (Challenge, error) {
+	readme, err := readFile(filepath.Join(path, "README.md"))
+}
+
+func readFile(path string) (string, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
 }
