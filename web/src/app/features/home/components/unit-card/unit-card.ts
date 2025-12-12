6f9189e20fb4564177e602207d20c72418d5d59a
Johrespi <johrespi@espol.edu.ec>
2025-12-12 02:18:34 -0500

    Add basic unit cards

--- web/src/app/features/home/components/unit-card/unit-card.ts
+++ web/src/app/features/home/components/unit-card/unit-card.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {Unit} from '../../models/unit';
 
 @Component({
   selector: 'app-unit-card',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './unit-card.html',
   styleUrl: './unit-card.scss',
 })
 export class UnitCard {
-
+  unit = input.required<Unit>();
 }
---
+++ web/src/app/features/home/models/unit.ts
@@ -0,0 +1,13 @@
+export interface Unit {
+  slug: string;
+  name: string;
+  description: string;
+}
+
+export const UNITS: Unit[] = [
+  {
+    slug: 'unit-1-intro',
+    name: 'Unit 1: Introducción a la Programación',
+    description: 'Aprende los fundamentos de Python'
+  },
+];
