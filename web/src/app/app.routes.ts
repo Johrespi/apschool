9500e0a43971e54325309f0df4d7d7670260dc3b
Johrespi <johrespi@espol.edu.ec>
2025-12-14 20:18:58 -0500

    refactor(router): use main layout route shell

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,7 +3,13 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/home/home').then(m => m.Home)
-  }
-
+    loadComponent: () =>
+      import('./shared/layouts/main-layout/main-layout').then(m => m.MainLayout),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/home/home').then(m => m.Home),
+      },
+    ],
+  },
 ];
