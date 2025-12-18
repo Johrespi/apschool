ebd30531219c009194e5496b57fc671dfab4fa0e
Johrespi <johrespi@espol.edu.ec>
2025-12-17 21:45:01 -0500

    inject authservice to navbar component

--- web/src/app/shared/components/navbar/navbar.ts
+++ web/src/app/shared/components/navbar/navbar.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth';
 
 
 @Component({
   selector: 'app-navbar',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-
+  protected readonly authService = inject(AuthService)
 }
