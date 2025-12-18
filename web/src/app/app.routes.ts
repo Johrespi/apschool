b6e0f7d4c5e9e556d7a921fd6e327a87967b56e8
Johrespi <johrespi@espol.edu.ec>
2025-12-17 21:41:24 -0500

    Add callback route

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'auth/callback',
+    loadComponent: () =>
+      import('./features/auth/callback').then(m => m.AuthCallback),
+  },
+  {
     path: '',
     loadComponent: () =>
       import('./shared/layouts/main-layout/main-layout').then(m => m.MainLayout),
