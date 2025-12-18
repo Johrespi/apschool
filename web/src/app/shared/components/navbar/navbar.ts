acff5d5bebb683f18d94eec67be80f7e6203f384
Johrespi <johrespi@espol.edu.ec>
2025-12-17 21:51:58 -0500

    wip: Add auth

--- web/src/app/shared/components/navbar/navbar.ts
+++ web/src/app/shared/components/navbar/navbar.ts
@@ -1,18 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
-import { AuthService } from '../../../core/services/auth';
 
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-navbar',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-  protected readonly authService = inject(AuthService)
+  protected readonly authService = inject(AuthService);
 }
