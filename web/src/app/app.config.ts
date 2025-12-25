dd96c03786eb3941279e0dd662657c05c74c511f
Johrespi <johrespi@espol.edu.ec>
2025-12-24 20:11:32 -0500

    feat(web): configure provideMarkdown in app.config

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth';
+import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideMarkdown(),
   ],
 };
